052c975592813f3465c2d1200aef316b7158e6b7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:23:19 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "web_pubsub_network_acls" {
+  description = <<EOT
+Map of web_pubsub_network_acls, attributes below
+Required:
+    - web_pubsub_id
+    - public_network (block):
+        - allowed_request_types (optional)
+        - denied_request_types (optional)
+Optional:
+    - default_action
+    - private_endpoint (block):
+        - allowed_request_types (optional)
+        - denied_request_types (optional)
+        - id (required)
+EOT
+
+  type = map(object({
+    web_pubsub_id  = string
+    default_action = optional(string, "Deny")
+    public_network = object({
+      allowed_request_types = optional(set(string))
+      denied_request_types  = optional(set(string))
+    })
+    private_endpoint = optional(object({
+      allowed_request_types = optional(set(string))
+      denied_request_types  = optional(set(string))
+      id                    = string
+    }))
+  }))
+}
+
