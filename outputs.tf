14194668af62fbf6622d7c50ca753fdfdd1377ef
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "web_pubsub_network_acls" {
-  description = "All web_pubsub_network_acl resources"
-  value       = azurerm_web_pubsub_network_acl.web_pubsub_network_acls
-}
 output "web_pubsub_network_acls_default_action" {
-  description = "List of default_action values across all web_pubsub_network_acls"
-  value       = [for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : v.default_action]
+  description = "Map of default_action values across all web_pubsub_network_acls, keyed the same as var.web_pubsub_network_acls"
+  value       = { for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : k => v.default_action }
 }
 output "web_pubsub_network_acls_private_endpoint" {
-  description = "List of private_endpoint values across all web_pubsub_network_acls"
-  value       = [for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : v.private_endpoint]
+  description = "Map of private_endpoint values across all web_pubsub_network_acls, keyed the same as var.web_pubsub_network_acls"
+  value       = { for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : k => v.private_endpoint }
 }
 output "web_pubsub_network_acls_public_network" {
-  description = "List of public_network values across all web_pubsub_network_acls"
-  value       = [for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : v.public_network]
+  description = "Map of public_network values across all web_pubsub_network_acls, keyed the same as var.web_pubsub_network_acls"
+  value       = { for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : k => v.public_network }
 }
 output "web_pubsub_network_acls_web_pubsub_id" {
-  description = "List of web_pubsub_id values across all web_pubsub_network_acls"
-  value       = [for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : v.web_pubsub_id]
+  description = "Map of web_pubsub_id values across all web_pubsub_network_acls, keyed the same as var.web_pubsub_network_acls"
+  value       = { for k, v in azurerm_web_pubsub_network_acl.web_pubsub_network_acls : k => v.web_pubsub_id }
 }
 
